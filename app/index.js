a43fe4593ea74503cc22f6ce7235d6ccf314f79c
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-08-02 15:35:54 +0530

    section-6 lecture 40

--- app/index.js
+++ app/index.js
@@ -1,1 +1,30 @@
 console.log("hello wepack");
+
+// const emails = ['akash@gmail.com', 'akash2@gmail.com', 'akash3@gmail.com']
+// emails.push('akash4@gmail.com')
+// console.log(emails);
+
+
+//block scoping
+const limit = 200;
+
+{
+  const limit = 10;
+  console.log('backstage limit', limit);
+}
+
+console.log('overall', limit);
+
+function hello(){
+  let msg = "hello from hello()"
+  console.log(msg);
+}
+
+
+function greeting(){
+  let msg = "hello from greeting()"
+  console.log(msg);
+}
+
+hello();
+greeting();
