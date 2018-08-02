c415dc506fdf52ddde8dc1eebda74e1c5ddb965f
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-08-02 13:22:16 +0530

    Section 4 Lecture 34

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'app'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'build')
+  }
+};
